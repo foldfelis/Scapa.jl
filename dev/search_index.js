a77ca037b5214a857af9a589b986c92c9e69c6c5
Documenter.jl <documenter@juliadocs.github.io>
2022-05-29 09:46:12 +0000

    build based on 641b46b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Scapa","category":"page"},{"location":"#Scapa","page":"Home","title":"Scapa","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Scapa.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Scapa]","category":"page"}]
+}
